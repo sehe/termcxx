ec972947966ac95a9186790595ae178e3774b4ca
Seth Heeren <sgheeren@gmail.com>
2015-10-22 00:33:10 +0200

    Cleanup grammar constructor

--- parser.cpp
+++ parser.cpp
@@ -9,8 +9,7 @@
 #include <fstream>
 
 namespace termcxx { namespace parser {
-    namespace qi    = boost::spirit::qi;
-    namespace ascii = boost::spirit::ascii;
+    namespace qi      = boost::spirit::qi;
 
     using Number      = long long;   // TODO FIXME SEHE - must support up to 99 digits (WTF)
     using Alias       = std::string;
@@ -51,74 +50,43 @@ namespace termcxx { namespace parser {
         {
             using namespace qi;
 
-            // When symbols is used for case-insensitive parsing (in a no_case directive), added symbol strings should
-            // be in lowercase. Symbol strings containing one or more uppercase characters will not match any input when
-            // symbols is used in a no_case directive.
+            // qi::symbols:
+            //      When symbols is used for case-insensitive parsing (in a no_case directive), added symbol strings
+            //      should be in lowercase. Symbol strings containing one or more uppercase characters will not match
+            //      any input when symbols is used in a no_case directive.
             control_chars.add // see https://en.wikipedia.org/wiki/C0_and_C1_control_codes
                 ("@", 0x00) ("a", 0x01) ("b", 0x02) ("c", 0x03) ("d",  0x04) ("e", 0x05) ("f", 0x06) ("g", 0x07)
                 ("h", 0x08) ("i", 0x09) ("j", 0x0a) ("k", 0x0b) ("l",  0x0c) ("m", 0x0d) ("n", 0x0e) ("o", 0x0f)
                 ("p", 0x10) ("q", 0x11) ("r", 0x12) ("s", 0x13) ("t",  0x14) ("u", 0x15) ("v", 0x16) ("w", 0x17)
                 ("x", 0x18) ("y", 0x19) ("z", 0x1a) ("[", 0x1b) ("\\", 0x1c) ("]", 0x1d) ("^", 0x1e) ("_", 0x1f)
-                // not technically part of the control characters but exists
+                // WP: not technically part of the control characters but exists
                 ("?", 0x7f);
 
-            eoleoi = eol | eoi;
+            auto white_space     = copy(lit(' ')|'\t');
+            eoleoi               = eol | eoi;
+            caret_escape         = '^' >> no_case[control_chars];
+            comma                = +(',' >> *white_space); // allow repeated `,,` as in line terminfo.ti:4603
 
-            // White space consists of the ' ' and <tab> character.
-            auto white_space = copy(lit(' ')|'\t');
-
-            // An ALIAS may contain any graph characters other than ',','/' and '|'. Graph characters are those
-            // characters for which isgraph() returns non-zero.
-            alias        = +(graph - char_(",/|")) >> &char_(",|");
-
-            // A LONGNAME may contain any print characters other than ',' and '|'. Print characters are those characters
-            // for which isprint() returns non-zero.
-            longname     = +(print - char_(",|"));
-
-            // Common part of all three types of features factored out.
-            feature_name = +(print - char_(",=#"));
-
-            // A BOOLEAN feature may contain any print characters other than ',', '=', and '#'.
-            boolean_feature = feature_name >> qi::attr(true);
-
-            // A NUMERIC feature consists of:
-            // a. A name which may contain any print character other than ',', '=', and '#'.
-            // b. The '#' character.
-            // c. A positive integer which conforms to the C language convention for integer constants.
-            numeric_feature = feature_name >> '#' >> int_;
-
-            // c. A string which may contain any print characters other than ','.  (see also string_feature)
-            caret_escape = '^' >> no_case[control_chars];
+            alias                = +(graph - char_(",/|")) >> &char_(",|");
+            longname             = +(print - char_(",|"));
+            feature_name         = +(print - char_(",=#"));
             string_feature_value = *(('\\' >> char_) | caret_escape | (print - ','));
 
-            // A STRING feature consists of:
-            // a. A name which may contain any print character other than ',', '=', and '#'.
-            // b. The '=' character.
-            // c. A string which may contain any print characters other than ','.
-            string_feature = feature_name >> '=' >> string_feature_value;
+            boolean_feature      = feature_name >> qi::attr(true);
+            numeric_feature      = feature_name >> '#' >> int_;
+            string_feature       = feature_name >> '=' >> string_feature_value;
 
-            // White space immediately following a ',' is ignored.
-            comma = +(',' >> *white_space); // allow repeated `,,` as in line terminfo.ti:4603
+            feature              = numeric_feature | string_feature | boolean_feature;
+            features             = +(!eol >> feature >> comma);
 
-            // feature : BOOLEAN | NUMERIC | STRING ;
-            feature = numeric_feature | string_feature | boolean_feature;
+            aliases              = alias % '|' >> -('|' >> longname);
+            header_line          = (aliases >> comma >> eol);
+            feature_line         = +white_space >> features >> eoleoi;
+            description          = header_line >> *feature_line;
 
-            // features : COMMA feature | features COMMA feature ;
-            features = +(!eol >> feature >> comma);
-
-            // aliases : ALIAS | aliases PIPE ALIAS ;
-            aliases = alias % '|' >> -('|' >> longname);
-
-            // Comments consist of <bol>, optional whitespace, a required '#', and a terminating <eol>.
-            comment_line
-                = (*blank >> '#' >> *(char_ - eol) >> eoleoi)
-                ;
-
-            empty_line   = !eoi >> *blank >> eoleoi;
-            header_line  = (aliases >> comma >> eol);
-            feature_line = +white_space >> features >> eoleoi;
-            description  = (header_line >> *feature_line);
-            descriptions = skip(comment_line | empty_line) [ *lexeme[ description ] ];
+            comment_line         = *blank >> '#' >> *(char_ - eol) >> eoleoi;
+            empty_line           = !eoi >> *blank >> eoleoi;
+            descriptions         = skip(comment_line | empty_line) [ *lexeme[ description ] ];
 
             BOOST_SPIRIT_DEBUG_NODES(
                     (eoleoi)(comment_line)(empty_line)(comma)(caret_escape)
