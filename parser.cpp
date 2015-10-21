1ca407767ae3c9928d967d8566fa7d572392b12b
Seth Heeren <sgheeren@gmail.com>
2015-10-21 22:01:58 +0200

    wip

--- parser.cpp
+++ parser.cpp
@@ -9,193 +9,176 @@
 #include <iterator>
 #include <fstream>
 
-namespace termcxx
-{
+namespace termcxx { namespace parser {
+    namespace qi    = boost::spirit::qi;
+    namespace ascii = boost::spirit::ascii;
+    namespace px    = boost::phoenix;
 
-namespace parser
-{
+    struct context
+    {
+        //int dummy;
 
-namespace qi = boost::spirit::qi;
-namespace ascii = boost::spirit::ascii;
-namespace px = boost::phoenix;
-
-//using qi::double_;
-using ascii::space;
-using ascii::blank;
-using ascii::graph;
-using ascii::print;
-//using px::ref;
-using px::construct;
-
-//using qi::eps;
-//using qi::lit;
-using qi::_val;
-using qi::_1;
-using ascii::char_;
-using qi::eol;
-using qi::eoi;
-using qi::int_;
-
-
-struct context
-{
-    int dummy;
-
-    context () = default;
-    context (context const &) = default;
-    context (std::vector<char> a)
-    { }
-    context (std::vector<char> a, std::vector<char> b)
-    { }
-};
-
+        //context () = default;
+        //context (context const &) = default;
+        //context (std::vector<char>) { }
+        //context (std::vector<char>, std::vector<char>) { }
+    };
 } }
 
+//BOOST_FUSION_ADAPT_STRUCT(termcxx::parser::context, dummy)
 
-BOOST_FUSION_ADAPT_STRUCT(
-    termcxx::parser::context,
-    (int, dummy))
+namespace termcxx { namespace parser {
 
+    template <typename Iterator, typename Skipper = qi::blank_type>
+    struct parser : qi::grammar<Iterator, context()>
+    {
+        parser() : parser::base_type(start)
+        {
+            using namespace qi;
 
-namespace termcxx
-{
+            eoleoi
+                = eol | eoi
+                ;
 
-namespace parser
-{
+            // White space consists of the ' ' and <tab> character.
+            white_space
+                = char_(' ') | char_('\t')
+                ;
 
-template <typename Iterator>
-struct parser
-    : qi::grammar<Iterator, context()>
-{
-    qi::rule<Iterator, std::string() > eoleoi
-    = eol | eoi
-        ;
+            // An ALIAS may contain any graph characters other than ',','/' and
+            // '|'. Graph characters are those characters for which isgraph() returns
+            // non-zero.
+            alias
+                = +(graph - char_(",/|"))
+                ;
 
-    // White space consists of the ' ' and <tab> character.
-    qi::rule<Iterator, std::string() > white_space
-    = char_(' ') | char_('\t')
-        ;
+            // A LONGNAME may contain any print characters other than ',' and
+            // '|'. Print characters are those characters for which isprint() returns
+            // non-zero.
+            longname
+                = +(print - (char_(',') | char_('|')))
+                ;
 
-    // An ALIAS may contain any graph characters other than ',','/' and
-    // '|'. Graph characters are those characters for which isgraph() returns
-    // non-zero.
-    qi::rule<Iterator, std::string() > alias
-    = +(graph - (char_(',') | char_('/') | char_('|')))
-        ;
+            // Common part of all three types of features factored out.
+            feature_name
+                = +(print - (char_(',') | char_('=') | char_('#')))
+                ;
 
-    // A LONGNAME may contain any print characters other than ',' and
-    // '|'. Print characters are those characters for which isprint() returns
-    // non-zero.
-    qi::rule<Iterator, std::string() > longname
-    = +(print - (char_(',') | char_('|')))
-        ;
+            // A BOOLEAN feature may contain any print characters other than ',', '=',
+            // and '#'.
+            boolean_feature
+                = feature_name
+                ;
 
-    // Common part of all three types of features factored out.
-    qi::rule<Iterator, std::string() > feature_name
-    = +(print - (char_(',') | char_('=') | char_('#')))
-        ;
+            // A NUMERIC feature consists of:
+            // a. A name which may contain any print character other than ',', '=', and
+            //    '#'.
+            // b. The '#' character.
+            // c. A positive integer which conforms to the C language convention for
+            //    integer constants.
+            numeric_feature
+                = feature_name >> char_('#') >> int_
+                ;
 
-    // A BOOLEAN feature may contain any print characters other than ',', '=',
-    // and '#'.
-    qi::rule<Iterator, std::string() > boolean_feature
-    = feature_name
-        ;
+            // c. A string which may contain any print characters other than ','.
+            // (see also string_feature)
+            string_feature_value
+                = +(print - char_(','))
+                ;
 
-    // A NUMERIC feature consists of:
-    // a. A name which may contain any print character other than ',', '=', and
-    //    '#'.
-    // b. The '#' character.
-    // c. A positive integer which conforms to the C language convention for
-    //    integer constants.
-    qi::rule<Iterator, std::string() > numeric_feature
-    = feature_name >> char_('#') >> int_
-        ;
+            // A STRING feature consists of:
+            // a. A name which may contain any print character other than ',', '=', and
+            //    '#'.
+            // b. The '=' character.
+            // c. A string which may contain any print characters other than ','.
+            string_feature
+                = feature_name >> char_('=') >> string_feature_value
+                ;
 
-    // c. A string which may contain any print characters other than ','.
-    // (see also string_feature)
-    qi::rule<Iterator, std::string() > string_feature_value
-    = +(print - char_(','))
-        ;
+            // White space immediately following a ',' is ignored.
+            comma
+                = char_(',') >> *white_space
+                ;
 
-    // A STRING feature consists of:
-    // a. A name which may contain any print character other than ',', '=', and
-    //    '#'.
-    // b. The '=' character.
-    // c. A string which may contain any print characters other than ','.
-    qi::rule<Iterator, std::string() > string_feature
-    = feature_name >> char_('=') >> string_feature_value
-        ;
+            // feature : BOOLEAN
+            //       | NUMERIC
+            //       | STRING
+            //       ;
+            feature
+                = boolean_feature
+                | numeric_feature
+                | string_feature
+                ;
 
-    // White space immediately following a ',' is ignored.
-    qi::rule<Iterator, std::string() > comma
-    = char_(',') >> *white_space
-        ;
+            // features : COMMA feature
+            //       | features COMMA feature
+            //       ;
+            features
+                = feature % comma
+                ;
 
-    // feature : BOOLEAN
-    //       | NUMERIC
-    //       | STRING
-    //       ;
-    qi::rule<Iterator, std::string() > feature
-    = boolean_feature
-        | numeric_feature
-        | string_feature
-        ;
+            // aliases : ALIAS // SEHE confusing, fixed?
+            //       | aliases PIPE ALIAS
+            //       ;
+            aliases
+                = alias % '|'
+                ;
 
-    // features : COMMA feature
-    //       | features COMMA feature
-    //       ;
-    qi::rule<Iterator, std::string() > features
-    = feature % comma
-        ;
+            // Comments consist of <bol>, optional whitespace, a required '#', and a
+            // terminating <eol>.
+            comment_line
+                = (*blank >> char_('#') >> *(char_ - eol) >> eoleoi)
+                ;
 
-    // aliases : PIPE ALIAS
-    //       | aliases PIPE ALIAS
-    //       ;
-    qi::rule<Iterator, std::string() > aliases
-    = alias % char_('|')
-        ;
+            empty_line
+                = ((+blank >> eoleoi)
+                        | eoleoi)
+                ;
 
-    // Comments consist of <bol>, optional whitespace, a required '#', and a
-    // terminating <eol>.
-    qi::rule<Iterator, std::string() > comment_line
-    = (*blank >> char_('#') >> *(char_ - eol) >> eoleoi)
-        ;
+            def_first_line
+                = (aliases >> eol)
+                ;
 
-    qi::rule<Iterator, std::string() > empty_line
-    = ((+blank >> eoleoi)
-        | eoleoi)
-        ;
+            def_subsequent_line
+                = (+white_space >> +features >> eoleoi)
+                ;
 
-    qi::rule<Iterator, std::string() > def_first_line
-    = (aliases >> eol)
-        ;
+            definition
+                = (def_first_line >> *def_subsequent_line)
+                ;
 
-    qi::rule<Iterator, std::string() > def_subsequent_line
-    = (+white_space >> +features >> eoleoi)
-        ;
+            start
+                = (*(comment_line
+                            | empty_line
+                            | definition)) [_val = px::construct<context> ()]
+                ;
+        }
 
-    qi::rule<Iterator, std::string() > definition
-    = (def_first_line >> *def_subsequent_line)
-        ;
+        qi::rule<Iterator, std::string()>  eoleoi;
+        qi::rule<Iterator, std::string()>  white_space;
+        qi::rule<Iterator, std::string()>  alias;
+        qi::rule<Iterator, std::string()>  longname;
+        qi::rule<Iterator, std::string()>  feature_name;
+        qi::rule<Iterator, std::string()>  boolean_feature;
+        qi::rule<Iterator, std::string()>  numeric_feature;
+        qi::rule<Iterator, std::string()>  string_feature_value;
+        qi::rule<Iterator, std::string()>  string_feature;
+        qi::rule<Iterator, std::string()>  comma;
+        qi::rule<Iterator, std::string()>  feature;
+        qi::rule<Iterator, std::string()>  features;
+        qi::rule<Iterator, std::string()>  aliases;
+        qi::rule<Iterator, std::string()>  comment_line;
+        qi::rule<Iterator, std::string()>  empty_line;
+        qi::rule<Iterator, std::string()>  def_first_line;
+        qi::rule<Iterator, std::string()>  def_subsequent_line;
+        qi::rule<Iterator, std::string()>  definition;
+        qi::rule<Iterator, context()>      start;
+    };
 
-    qi::rule<Iterator, context()> start
-    = (*(comment_line
-            | empty_line
-            | definition))[_val = construct<context> ()]
-        ;
+    //template struct parser<std::string::iterator>;
+    //template struct parser<boost::spirit::istream_iterator>;
 
-    parser()
-        : parser::base_type(start)
-    { }
-};
-
-//template struct parser<std::string::iterator>;
-//template struct parser<std::istreambuf_iterator<char> >;
-
-} // namespace parser
-
-} // namespace termcxx
-
+} } // namespace parser, termcxx
 
 int
 main ()
@@ -205,7 +188,7 @@ main ()
     parser p;
 
     std::ifstream f ("termtypes.ti");
-    It first(f >> std::nosk), end;
+    It first(f >> std::noskipws), end;
     bool ok = boost::spirit::qi::parse(first, end, p);
 
     if (ok)
